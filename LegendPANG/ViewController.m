62e0a7a372cb1b8db45320b4c706bf61aa80460a
Dalang <2387623367@qq.com>
2016-10-31 09:41:00 +0800

    test timerlabel

--- LegendPANG/ViewController.m
+++ LegendPANG/ViewController.m
@@ -18,11 +18,33 @@
     [super viewDidLoad];
     [self showTimerLabel];
 }
+#pragma mark - Test TimerLabel
 -(void)showTimerLabel{
     _timerLabel = [[TimerLabel alloc] initWithFrame:CGRectMake(100, 50, 100, 50)];
     _timerLabel.backgroundColor = [UIColor redColor];
     [self.view addSubview:_timerLabel];
-    //[_timerLabel beginCount];
+    
+    UIButton * beginBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    beginBtn.frame = CGRectMake(50, 200, 50, 20);
+    [beginBtn addTarget:self action:@selector(beginCountClick:) forControlEvents:UIControlEventTouchUpInside];
+    [beginBtn setTitle:@"begin" forState:UIControlStateNormal];
+    beginBtn.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:beginBtn];
+    
+    UIButton * stopBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    stopBtn.frame = CGRectMake(150, 200, 50, 20);
+    [stopBtn addTarget:self action:@selector(stopCountClick:) forControlEvents:UIControlEventTouchUpInside];
+    [stopBtn setTitle:@"stop" forState:UIControlStateNormal];
+    stopBtn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:stopBtn];
+
+   
+}
+-(void)beginCountClick:(UIButton *)btn{
+     [_timerLabel beginCount];
+}
+-(void)stopCountClick:(UIButton *)btn{
+     [_timerLabel stopCount];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
